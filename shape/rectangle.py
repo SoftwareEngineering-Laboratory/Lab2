ca62cf455ce75768933fef05f18f8781151351bf
amirsadra.abdollahi <amirsadra.abdollahi@cafebazaar.ir>
2023-07-21 16:54:15 +0330

    Add set and get in Rectangle
    Add methods set and get for width and height

--- shape/rectangle.py
+++ shape/rectangle.py
@@ -6,3 +6,15 @@ class Rectangle:
 
     def compute_area(self):
         return self.height * self.width
+
+    def get_width(self):
+        return self.width
+
+    def set_width(self, new_width):
+        self.width = new_width
+
+    def get_height(self):
+        return self.height
+
+    def set_height(self, new_height):
+        self.height = new_height
