dc244c2f4c4ce44aef21abb76f2d13843a07e656
amirsadra.abdollahi <amirsadra.abdollahi@cafebazaar.ir>
2023-07-21 16:51:12 +0330

    Add tests get_set for width and height Rectangle

--- shape/test.py
+++ shape/test.py
@@ -12,3 +12,33 @@ class TestRectangle(unittest.TestCase):
 
         r = Rectangle(6, 7)
         self.assertEqual(r.compute_area(), 42)
+
+    def test_get_set_width(self):
+        r = Rectangle(3, 4)
+        r.set_width(6)
+        self.assertEqual(r.get_width(), 6)
+
+        r = Rectangle(5, 5)
+        r.set_width(2)
+        self.assertEqual(r.get_width(), 2)
+
+        r = Rectangle(7, 5)
+        r.set_width(3)
+        self.assertEqual(r.get_width(), 3)
+        r.set_width(8)
+        self.assertEqual(r.get_width(), 8)
+
+    def test_get_set_height(self):
+        r = Rectangle(3, 4)
+        r.set_height(2)
+        self.assertEqual(r.get_height(), 2)
+
+        r = Rectangle(5, 5)
+        r.set_height(4)
+        self.assertEqual(r.get_height(), 4)
+
+        r = Rectangle(7, 5)
+        r.set_height(4)
+        self.assertEqual(r.get_height(), 4)
+        r.set_height(8)
+        self.assertEqual(r.get_height(), 8)
