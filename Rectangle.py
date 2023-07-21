428c9ce29ef7632cc049e084ec675716dd3d5dc9
amirsadra.abdollahi <amirsadra.abdollahi@cafebazaar.ir>
2023-07-21 16:43:53 +0330

    Refactor Rectangle class for testing

--- Rectangle.py
+++
@@ -1,9 +0,0 @@
-class Rectangle:
-
- def __init__(self, height, width):
-  self.height = height
-  self.width = width
-
- def computeArea(self):
-  return height * width
-  
--- shape/test.py
+++ shape/test.py
@@ -1,4 +1,5 @@
 import unittest
+from shape import Rectangle
 
 
 class TestRectangle(unittest.TestCase):
